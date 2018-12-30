fc475414b489551c3dec147548010480c835e4c9
Kathleen Liang <43657698+kathleen-liang@users.noreply.github.com>
2018-12-30 18:22:58 -0500

    Update gameStats.py

--- gameStats.py
+++ gameStats.py
@@ -6,8 +6,12 @@ class GameStats():
         self.ka_settings = ka_settings
         self.reset_stats()
         #Start in active state
-        self.game_active = True
+        self.game_active = False
+        #High score
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize stats that change during game"""
         self.koalas_left = self.ka_settings.koala_limit
+        self.score = 0
+        self.level = 1
